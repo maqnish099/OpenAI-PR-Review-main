f3d8b533c59d9f930e23f375fad10251519e9421
maqnish099 <maqnish@gmail.com>
2026-03-11 09:26:43 +0530

    Update ApparelSizeProductReferenceTargetStrategy.java
    added the changes

--- custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
+++ custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
@@ -125,6 +125,7 @@ public class ApparelSizeProductReferenceTargetStrategy implements ProductReferen
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
 			System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+			 System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
 
 
         }
